1803a2dff0205b89dae8f40368d7b340ecc10a1d
hadesshark <hades_shark@hotmail.com>
2016-04-03 11:02:57 +0800

    重構 jsonTED

--- jsonTED.py
+++ jsonTED.py
@@ -1,40 +1,40 @@
 # -*- coding: utf8 -*-
 import json
 import urllib.request as request
-import pprint
 
-class Setting(object):
-    def __init__(self, url="http://ck101.com/forum-3419-1.html"):
-        self.url = url
 
-    def get_res(self):
-        return request.urlopen(self.url)
+def getjsoncapions(url):
+    return json.loads(request.urlopen(url).read().decode('utf-8'))['captions']
 
-    # is str
-    def requests_text(self):
-        return self.get_res().read()
+
+def isstartOfParagraph(item):
+    return True if item['startOfParagraph'] else False
+
+
+def content(item):
+    return item['content']
+
+
+def getcontents(url):
+    capions = getjsoncapions(url)
+    temp_content = ''
+    for item in capions:
+        if isstartOfParagraph(item):
+            temp_content += '\n\n'
+        temp_content += content(item)
+    return temp_content
 
 
 def main():
     en_url = "http://www.ted.com/talks/subtitles/id/33/lang/en"
     tw_url = "http://www.ted.com/talks/subtitles/id/33/lang/zh-tw"
-    en_data = json.loads(Setting(en_url).requests_text().decode('utf-8'))
-    tw_data = json.loads(Setting(tw_url).requests_text().decode('utf-8'))
-    contest = ''
-    for index in range(len(en_data['captions'])):
-    	if en_data['captions'][index]['startOfParagraph']:
-    		contest += '\n\n'
+    content = ''
 
-    		# if tw_data['captions'][index]['startOfParagraph']:
-	    	# 	contest += '\n\n'
-	    	# contest += tw_data['captions'][index]['content']
-
-    	contest += en_data['captions'][index]['content']
-	    	
+    content += getcontents(en_url)
+    content += getcontents(tw_url)
 
     with open("test_TED.txt", "wb") as txt_file:
-    	txt_file.write(contest.encode('utf-8'))
-
+        txt_file.write(content.encode('utf-8'))
 
 if __name__ == '__main__':
     main()
